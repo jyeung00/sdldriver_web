458a1cb1124816d63c9834770c4ca635a1d4923f
jyeung5 <jyeung5@MGC000001561>
2017-02-08 14:16:07 -0500

    errors

--- Firebase.js
+++ Firebase.js
@@ -561,3 +561,17 @@ function readCookie(name) {
 function eraseCookie(name) {
     createCookie(name,"",-1);
 }
+
+function getURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}​
