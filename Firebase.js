7e807921bd06c37e7d8d9166065b0d33335975d9
jyeung5 <jyeung5@MGC000001561>
2017-02-08 16:03:14 -0500

    d

--- Firebase.js
+++ Firebase.js
@@ -203,7 +203,7 @@ function postToChatbox(message, alignment){
 }
 
 function sendChat(){
-    message = document.getElementById("chatbox").getValue();
+    message = document.getElementById("chatbox").value;
     document.getElementById("chatbox").setValue("") ;  
 
     var postData = {};
